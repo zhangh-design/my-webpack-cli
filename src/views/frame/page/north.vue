0e75156fb200c8fec50be65ac3717106a10f0609
zhangh <506365426@qq.com>
2020-05-14 15:38:43 +0800

    docs: 测试 sass-resources-loader 载入的全局变量

--- src/views/frame/page/north.vue
+++ src/views/frame/page/north.vue
@@ -1,8 +1,13 @@
 <template>
   <div :class="$style.northHeader">
     <div>图片1111</div>
-    <div>menu</div>
+    <div class="bbbbb">
+      menu
+    </div>
     <div>登出xtong</div>
+    <p class="cccc">
+      胜多负少第三方第三方
+    </p>
   </div>
 </template>
 
@@ -15,9 +20,28 @@ export default {
 }
 </script>
 
+<style lang="less" scoped>
+@import "./css/north.less";
+.bbbbb{
+  color: @color;
+}
+</style>
+
 <style lang="less" module>
-.north-header{
+// @import "@assets/css/less/variables.less";
+@assets: '~@assets/img/frame/';
+.north-header {
   display: flex;
   flex-direction: row;
+}
+.north-header div:first-child {
+  color: @color;
+  background-image: url('@{assets}avatar.jpg');
+  background-position: 50% 50%;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+.north-header div:last-child {
+  color: @color;
 }
 </style>
