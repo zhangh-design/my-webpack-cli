3a21b1e006fd3cb3e0aa190a4b73f8d84ecaf253
zhangh <506365426@qq.com>
2020-05-14 18:30:41 +0800

    docs: 引入双飞翼布局

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from '@/router/index.js'
 import store from '@/store/index.js'
 import inject from '@plugins/inject.js'
 import { Message, Input, Button, Container, Header, Aside, Main, Footer } from 'element-ui'
-import { FastTextInput, FastButton, FastPanel, FastBorderLayout } from 'fast-element-ui'
+import { FastTextInput, FastButton, FastPanel, FastBorderLayout, FastDoubleWingLayout } from 'fast-element-ui'
 import 'fast-element-ui/lib/theme-default/index.css'
 import '@assets/css/main.css'
 
@@ -12,6 +12,7 @@ Vue.use(inject)
 Vue.use(FastTextInput, Input)
 Vue.use(FastButton, Button)
 Vue.use(FastPanel, [Container, Header, Aside, Main, Footer, FastBorderLayout])
+Vue.use(FastDoubleWingLayout)
 Vue.prototype.$message = Message
 
 new Vue({
