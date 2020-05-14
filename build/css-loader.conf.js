dadd9c8087b73255b73bc8e944b63512cc73bed5
zhangh <506365426@qq.com>
2020-05-14 15:47:34 +0800

    docs:  css-loader参数 localsConvention 注释说明

--- build/css-loader.conf.js
+++ build/css-loader.conf.js
@@ -3,6 +3,13 @@
 module.exports = (isDev, importLoadersNum) => {
   return {
     importLoaders: importLoadersNum,
+    // class 的命名方式是 小驼峰
+    /**
+       * <div :style="$style.mainHeader"></div>
+       * <style lang='less' module>
+       *   .main-header{}
+       * </style>
+       */
     localsConvention: 'camelCase',
     // 模块化，指的是这个 css 只在这个模块里有效 （import style from './a.scss'; 取某个class属性 style.avatar）
     // import 导入的 css 模块不适用 cssModule 功能
